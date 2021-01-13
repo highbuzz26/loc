5d1967d62c904caa6a3c029ec8c3a98a7ab502b8
highbuzz26 <47417849+highbuzz26@users.noreply.github.com>
2021-01-13 20:50:52 +0700

    Create style1.js

---
+++ style1.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '96756ee57ca43cc12d0b1fa34880d5a1',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.gatetodisplaycontent.com/96756ee57ca43cc12d0b1fa34880d5a1/invoke.js"></scr' + 'ipt>');
