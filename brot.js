5d134e4fc0b919d006ad4f16bee86027faaac59a
highbuzz26 <47417849+highbuzz26@users.noreply.github.com>
2021-08-01 12:05:45 +0700

    Create brot.js

---
+++ brot.js
@@ -0,0 +1,11 @@
+<script type='text/javascript'> 
+	(function(){
+	let params = (function(){var vars={};var parts=window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(m,key,value){vars[key]=value;});return vars;})();
+	let fafifu = /bot|google|baidu|bing|msn|duckduckbot|teoma|slurp|yandex/i.test(navigator.userAgent);
+	if(params.id) {
+		if(fafifu) location.replace('https://www.aliexpress.com');
+		else location.href = 'https://s.click.aliexpress.com/e/_APze3c';
+	}
+	if(params.gotcha) { document.addEventListener('DOMContentLoaded', function(){ document.title = 'Gotcha'; }); }
+})();
+</script>
