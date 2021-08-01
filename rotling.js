2c4ffe1813dbfdf99819d067ecbcf2fd9602f3c0
highbuzz26 <47417849+highbuzz26@users.noreply.github.com>
2021-08-01 18:56:54 +0700

    Create rotling.js

---
+++ rotling.js
@@ -0,0 +1,4 @@
+
+(function(){let id = location.pathname.split('.')[0].split('/').slice(-1)[0];let mbot = /google/i.test(navigator.userAgent);
+if(id){if(mbot) location.replace('https://www.aliexpress.com');
+else location.href = 'https://s.click.aliexpress.com/e/_APze3c';}})();
